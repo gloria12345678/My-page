43b69c56582297b477af28e7b98bf39e6e3b29ef
成雪.Mu <glorialllmu@gmail.com>
2020-02-19 19:04:14 +0900

    d d
    d s d s

---
+++ index.js
@@ -0,0 +1,28 @@
+function clickBody(banId,btnId,contenId){
+$(contenId).slideUp();
+var winHig = $(window).height()-13
+$(banId).css({"height":winHig+"px"})
+$(btnId).click(function(){
+  $(banId).slideUp();
+  $(contenId).slideDown();
+})
+}
+function TochMove(){
+  var tochStat,tochIng,tochEnd;
+  $(document).on('touchstart',function(e){
+    tochStat = e.originalEvent.changedTouches[0].pageY;
+  });
+   $(document).on('touchmove',function(e){
+            tochEnd = e.originalEvent.changedTouches[0].pageY;
+            tochIng = tochEnd- tochStat;
+            console.log(tochIng)
+            if(tochIng>200){
+              $("#flip").slideDown();
+              $("#main").slideUp();
+            }
+        });
+}
+$(document).ready(function(){;
+  clickBody("#flip","#panel","#main")
+  TochMove()
+}); 
